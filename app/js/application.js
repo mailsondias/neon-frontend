0c3363af7e4550d966943f370bf4aaac71a8674b
Mailson Dias <mamagt@gmail.comm>
2016-11-17 08:04:39 -0200

    Closed #1

--- app/js/application.js
+++ app/js/application.js
@@ -1,73 +1,23 @@
-(function($){
+/**
+ * Lib ClassList
+ */
+if("document"in self){if(!("classList"in document.createElement("_"))||document.createElementNS&&!("classList"in document.createElementNS("http://www.w3.org/2000/svg","g"))){(function(t){"use strict";if(!("Element"in t))return;var e="classList",i="prototype",n=t.Element[i],s=Object,r=String[i].trim||function(){return this.replace(/^\s+|\s+$/g,"")},a=Array[i].indexOf||function(t){var e=0,i=this.length;for(;e<i;e++){if(e in this&&this[e]===t){return e}}return-1},o=function(t,e){this.name=t;this.code=DOMException[t];this.message=e},l=function(t,e){if(e===""){throw new o("SYNTAX_ERR","An invalid or illegal string was specified")}if(/\s/.test(e)){throw new o("INVALID_CHARACTER_ERR","String contains an invalid character")}return a.call(t,e)},c=function(t){var e=r.call(t.getAttribute("class")||""),i=e?e.split(/\s+/):[],n=0,s=i.length;for(;n<s;n++){this.push(i[n])}this._updateClassName=function(){t.setAttribute("class",this.toString())}},u=c[i]=[],f=function(){return new c(this)};o[i]=Error[i];u.item=function(t){return this[t]||null};u.contains=function(t){t+="";return l(this,t)!==-1};u.add=function(){var t=arguments,e=0,i=t.length,n,s=false;do{n=t[e]+"";if(l(this,n)===-1){this.push(n);s=true}}while(++e<i);if(s){this._updateClassName()}};u.remove=function(){var t=arguments,e=0,i=t.length,n,s=false,r;do{n=t[e]+"";r=l(this,n);while(r!==-1){this.splice(r,1);s=true;r=l(this,n)}}while(++e<i);if(s){this._updateClassName()}};u.toggle=function(t,e){t+="";var i=this.contains(t),n=i?e!==true&&"remove":e!==false&&"add";if(n){this[n](t)}if(e===true||e===false){return e}else{return!i}};u.toString=function(){return this.join(" ")};if(s.defineProperty){var h={get:f,enumerable:true,configurable:true};try{s.defineProperty(n,e,h)}catch(d){if(d.number===-2146823252){h.enumerable=false;s.defineProperty(n,e,h)}}}else if(s[i].__defineGetter__){n.__defineGetter__(e,f)}})(self)}else{(function(){"use strict";var t=document.createElement("_");t.classList.add("c1","c2");if(!t.classList.contains("c2")){var e=function(t){var e=DOMTokenList.prototype[t];DOMTokenList.prototype[t]=function(t){var i,n=arguments.length;for(i=0;i<n;i++){t=arguments[i];e.call(this,t)}}};e("add");e("remove")}t.classList.toggle("c3",false);if(t.classList.contains("c3")){var i=DOMTokenList.prototype.toggle;DOMTokenList.prototype.toggle=function(t,e){if(1 in arguments&&!this.contains(t)===!e){return e}else{return i.call(this,t)}}}t=null})()}}
+
+/**
+ * App NEON
+ */
+(function(){
 
 	'use strict'
 
-		var width_window,
-			width_window = $(document).width()
+	var btnMenu = document.getElementById('btn-menu'),
+		menu    = document.getElementsByClassName('menu')[0]
 
-	// Navbar fixo ao rolar a tela
-		// $(window).resize(function(){
-		// 	width_window = $(document).width()
-		// })
+	btnMenu.onclick = function(e) {
+		e.preventDefault()
 
-		// $(window).scroll(function(){
-		// 	var $nav 	= $('.nav-primary').offset(),
-		// 		topNav 	= 183
-
-		// 	if ($(this).scrollTop() >= topNav) {
-		// 		$('.shadow-nav-primary').removeClass('hide')
-		// 		$('.nav-primary').addClass('fixed')
-		// 	} else {
-		// 		$('.shadow-nav-primary').addClass('hide')
-		// 		$('.nav-primary').removeClass('fixed')
-		// 	}
-		// })
-	// end: Navbar fixo ao rolar a tela
-
-	if (window.location.pathname == "/index.html" || window.location.pathname == "" || window.location.pathname == "/") {
-		setTimeout(function(){
-			$('#loading').hide()
-		},1000)
+		btnMenu.classList.toggle('-open')
+		menu.classList.toggle('-open')
 	}
 
-	$(window).load(function(){
-		if (window.location.pathname == "/index.html" || window.location.pathname == "" || window.location.pathname == "/") {
-			$('#loading').remove()
-		}
-
-		// bindTabInformations()
-
-		// var winH = $(window).scrollTop(),
-		// 	disH = $(window).height() + 50,
-		// 	$btnCall = $('#btn-call')
-
-		// if (winH > disH) {
-		//     $btnCall.addClass('available')
-		// } else {
-		//     $btnCall.removeClass('available')
-		// }
-	})
-
-	$(function(){
-		$('#menu-button').on('click',function(e){
-			e.preventDefault()
-
-			$(this).toggleClass('checked')
-			$('.nav-mobile').toggleClass('is-active')
-			if( $(this).hasClass('checked') ){
-				$('body, html').toggleClass('no-scrolling')
-			} else {
-				$('body, html').toggleClass('no-scrolling')
-			}
-		})
-
-		$('.nav-mobile .btn-treatments').on('click',function(e){
-			e.preventDefault()
-
-			$(this).parent('.item-treatments').find('.nav-submenu').toggleClass('open')
-		})
-
-		
-	})
-
-})(jQuery)
+})()
--- build/js/application.js
+++ build/js/application.js
@@ -1,73 +1,111 @@
-(function($){
+/**
+ * Lib ClassList
+ */
+if("document"in self){if(!("classList"in document.createElement("_"))||document.createElementNS&&!("classList"in document.createElementNS("http://www.w3.org/2000/svg","g"))){(function(t){"use strict";if(!("Element"in t))return;var e="classList",i="prototype",n=t.Element[i],s=Object,r=String[i].trim||function(){return this.replace(/^\s+|\s+$/g,"")},a=Array[i].indexOf||function(t){var e=0,i=this.length;for(;e<i;e++){if(e in this&&this[e]===t){return e}}return-1},o=function(t,e){this.name=t;this.code=DOMException[t];this.message=e},l=function(t,e){if(e===""){throw new o("SYNTAX_ERR","An invalid or illegal string was specified")}if(/\s/.test(e)){throw new o("INVALID_CHARACTER_ERR","String contains an invalid character")}return a.call(t,e)},c=function(t){var e=r.call(t.getAttribute("class")||""),i=e?e.split(/\s+/):[],n=0,s=i.length;for(;n<s;n++){this.push(i[n])}this._updateClassName=function(){t.setAttribute("class",this.toString())}},u=c[i]=[],f=function(){return new c(this)};o[i]=Error[i];u.item=function(t){return this[t]||null};u.contains=function(t){t+="";return l(this,t)!==-1};u.add=function(){var t=arguments,e=0,i=t.length,n,s=false;do{n=t[e]+"";if(l(this,n)===-1){this.push(n);s=true}}while(++e<i);if(s){this._updateClassName()}};u.remove=function(){var t=arguments,e=0,i=t.length,n,s=false,r;do{n=t[e]+"";r=l(this,n);while(r!==-1){this.splice(r,1);s=true;r=l(this,n)}}while(++e<i);if(s){this._updateClassName()}};u.toggle=function(t,e){t+="";var i=this.contains(t),n=i?e!==true&&"remove":e!==false&&"add";if(n){this[n](t)}if(e===true||e===false){return e}else{return!i}};u.toString=function(){return this.join(" ")};if(s.defineProperty){var h={get:f,enumerable:true,configurable:true};try{s.defineProperty(n,e,h)}catch(d){if(d.number===-2146823252){h.enumerable=false;s.defineProperty(n,e,h)}}}else if(s[i].__defineGetter__){n.__defineGetter__(e,f)}})(self)}else{(function(){"use strict";var t=document.createElement("_");t.classList.add("c1","c2");if(!t.classList.contains("c2")){var e=function(t){var e=DOMTokenList.prototype[t];DOMTokenList.prototype[t]=function(t){var i,n=arguments.length;for(i=0;i<n;i++){t=arguments[i];e.call(this,t)}}};e("add");e("remove")}t.classList.toggle("c3",false);if(t.classList.contains("c3")){var i=DOMTokenList.prototype.toggle;DOMTokenList.prototype.toggle=function(t,e){if(1 in arguments&&!this.contains(t)===!e){return e}else{return i.call(this,t)}}}t=null})()}}
+
+/**
+ * App NEON
+ */
+(function(){
 
 	'use strict'
 
-		var width_window,
-			width_window = $(document).width()
+	var btnMenu = document.getElementById('btn-menu'),
+		menu    = document.getElementsByClassName('menu')[0]
 
-	// Navbar fixo ao rolar a tela
-		// $(window).resize(function(){
-		// 	width_window = $(document).width()
-		// })
+	btnMenu.onclick = function(e) {
+		e.preventDefault()
 
-		// $(window).scroll(function(){
-		// 	var $nav 	= $('.nav-primary').offset(),
-		// 		topNav 	= 183
-
-		// 	if ($(this).scrollTop() >= topNav) {
-		// 		$('.shadow-nav-primary').removeClass('hide')
-		// 		$('.nav-primary').addClass('fixed')
-		// 	} else {
-		// 		$('.shadow-nav-primary').addClass('hide')
-		// 		$('.nav-primary').removeClass('fixed')
-		// 	}
-		// })
-	// end: Navbar fixo ao rolar a tela
-
-	if (window.location.pathname == "/index.html" || window.location.pathname == "" || window.location.pathname == "/") {
-		setTimeout(function(){
-			$('#loading').hide()
-		},1000)
+		btnMenu.classList.toggle('-open')
+		menu.classList.toggle('-open')
 	}
 
-	$(window).load(function(){
-		if (window.location.pathname == "/index.html" || window.location.pathname == "" || window.location.pathname == "/") {
-			$('#loading').remove()
-		}
+})()
+function initialize() {
 
-		// bindTabInformations()
+  // Exibir mapa;
+  var myLatlng = new google.maps.LatLng(-15.7362495,-47.896529,17);
+  var mapOptions = {
+    zoom: 16,
+    scrollwheel: false,
+    center: myLatlng,
+    panControl: false,
+    // mapTypeId: google.maps.MapTypeId.SATELLITE
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+    // mapTypeControlOptions: {
+    //   mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+    // }
+  }
 
-		// var winH = $(window).scrollTop(),
-		// 	disH = $(window).height() + 50,
-		// 	$btnCall = $('#btn-call')
+  // Parâmetros do texto que será exibido no clique;
+  var contentString = '<h4 style="font-weight: bold; font-size: 1.6em; color: #333;">Artros Ortopedia</h4>' +
+                      '<p style="color: #333; padding: 0.3em 0; font-size: 1.2em">Ed. Multiclínicas - Sala 14</p>';
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      maxWidth: 700
+  });
 
-		// if (winH > disH) {
-		//     $btnCall.addClass('available')
-		// } else {
-		//     $btnCall.removeClass('available')
-		// }
-	})
+  // Exibir o mapa na div #mapa;
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-	$(function(){
-		$('#menu-button').on('click',function(e){
-			e.preventDefault()
+  // Marcador personalizado;
+  var image = 'http://www.bettainterativa.com.br/jobs/artros/images/pin-map.png';
+  var marcadorPersonalizado = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      icon: image,
+      title: 'Parthenon Festas',
+      animation: google.maps.Animation.DROP
+  });
 
-			$(this).toggleClass('checked')
-			$('.nav-mobile').toggleClass('is-active')
-			if( $(this).hasClass('checked') ){
-				$('body, html').toggleClass('no-scrolling')
-			} else {
-				$('body, html').toggleClass('no-scrolling')
-			}
-		})
+  // Exibir texto ao clicar no ícone;
+  google.maps.event.addListener(marcadorPersonalizado, 'click', function() {
+    infowindow.open(map,marcadorPersonalizado);
+  });
 
-		$('.nav-mobile .btn-treatments').on('click',function(e){
-			e.preventDefault()
 
-			$(this).parent('.item-treatments').find('.nav-submenu').toggleClass('open')
-		})
+  // Estilizando o mapa;
+    // Criando um array com os estilos
+    var styles = [
+      {
+        stylers: [
+          { hue: "#d5e9d2" },
+          { saturation: 0 },
+          { lightness: 0 },
+          { gamma: 1 }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          { lightness: 100 },
+          { visibility: "simplified" }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "labels"
+      }
+    ];
 
-		
-	})
+    // crio um objeto passando o array de estilos (styles) e definindo um nome para ele;
+    var styledMap = new google.maps.StyledMapType(styles, {
+      name: "Artros Ortopedia"
+    });
 
-})(jQuery)
+    // Aplicando as configurações do mapa
+    // map.mapTypes.set('map_style', styledMap);
+    // map.setMapTypeId('map_style');
+
+  }
+
+// Função para carregamento assíncrono
+function loadScript() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyDeHb17So0QupSGO_d6b8X-OyvJ32UQehs&callback=initialize";
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
